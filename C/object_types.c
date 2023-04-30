e9d9fb7d13a1ebd5d04bff4567e90bd78de50b22
Supinesunhinged <ryanalvarado.dev@gmail.com>
2023-04-29 20:24:21 -0500

    Commit

---
+++ C/object_types.c
@@ -0,0 +1,36 @@
+#include <stdbool.h>
+
+// Boolean types
+
+_Bool flag1 = 0;
+bool flag2 = false;
+
+// Character Types
+char
+signed char
+unsigned char
+wchar_t // Wide character type
+
+// Numerical Types
+    // Integer Types
+        signed char
+        short int
+        int
+        long int
+        long long int
+        unsigned char
+        unsigned short int
+        unsigned int
+        unsigned long int
+        unsigned long long int
+    // Enum Types
+    enum day {sun, mon, tue, wed, thu, fri, sat};
+    enum cardinal_points {north = 0, east = 90, south = 180, west = 270};
+    enum months {jan = 1, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec};
+    // Floating-Point Types
+    float
+    double
+    long double
+    // void Types
+    void * // reference any object
+    void
